5eac3b45cf501af1fb941cb439c7fbbf0b461237
egbejob680 <jobegbe39@gmail.com>
2023-01-04 18:28:38 +0100

    last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    if number > -1:
+        lastdigit = number % 10
+    else:
+        lastdigit = (-number) % 10
+    print(str(lastdigit), end='')
+    return 
+    
