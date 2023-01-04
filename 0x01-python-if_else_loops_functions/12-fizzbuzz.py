edf81478aaea042d867be319658d8f7eca14580c
egbejob680 <jobegbe39@gmail.com>
2023-01-04 18:31:41 +0100

    12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for k in range(1, 100 + 1):
+        if k % 3 == 0 and k % 5 == 0:
+            print('FizzBuzz', end=' ')
+        elif k % 3 == 0:
+            print('Fizz', end=' ')
+        elif k % 5 == 0 and k == 100:
+            print('Buzz', end=' ')
+        elif k % 5 == 0:
+            print('Buzz', end=' ')
+        else:
+            print('{:d}'.format(k), end=' ')
