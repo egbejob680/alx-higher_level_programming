b7d18aebba4f96566e76ace35aedd1bc31fcdc22
egbejob680 <jobegbe39@gmail.com>
2023-04-12 23:28:35 +0100

    warm up

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/**
+ * add - Computes the sum of 2 numbers.
+ * @param {Number} a - The first number.
+ * @param {Number} b - The second number.
+ *
+ * @returns The sum of the 2 numbers.
+ */
+function add (a, b) {
+  return a + b;
+}
+
+console.log(
+  add(Number.parseInt(process.argv[2]), Number.parseInt(process.argv[3]))
+);
+/*function myFunction(a,b){
+    return a + b  
+
+}
+console.log(myFunction(1,7))*/
