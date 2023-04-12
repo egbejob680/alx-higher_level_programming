2e49b1e60edc49b708ee559121745d06c83b5d25
egbejob680 <jobegbe39@gmail.com>
2023-04-12 22:51:47 +0100

    mul

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const myVar = "C is fun\nPython is cool\nJavaScript is amazing"
-console.log(myVar)
+/*const myVar = "C is fun\nPython is cool\nJavaScript is amazing"
+console.log(myVar)*/
+['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(
+  phrase => console.log(phrase)
+);
